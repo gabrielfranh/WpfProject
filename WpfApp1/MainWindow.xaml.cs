61db433b05442c0276074d784c671d5e3f3b8557
Gabriel Habermann <gabrielfranh@outlook.com>
2021-03-07 00:52:40 -0300

    Creating project structure

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfApp1.ViewModels;
 
 namespace WpfApp1
 {
@@ -20,9 +21,24 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        ClienteViewModel viewModel = new MainViewModel();
+
         public MainWindow()
         {
+            DataContext = viewModel;
+
             InitializeComponent();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            viewModel.BotaoAcionado();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
